5b25b02850915bccf23812b77d6ed523b029ca78
1eof <100410797+1eof@users.noreply.github.com>
2024-07-05 14:41:30 +0800

    Add comment.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,14 @@ local keymap = vim.keymap
 -- Leader Key
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
--- nvim-tree
+-- Tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
--- telescope
+-- Telescope
 keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
 keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
 keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>")
+-- Lazy
+keymap.set("n", "<leader>lz", ":Lazy<CR>")
+-- Mason
+keymap.set("n", "<leader>ms", ":Mason<CR>")
