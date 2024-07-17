c4cc93b5356957187af888c1e4ffed2e0ab37edf
1eof <100410797+1eof@users.noreply.github.com>
2024-07-17 17:39:22 +0800

    Update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,4 +2,14 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      offsets = {
+        filetype = "neo-tree",
+        text = "Neotree",
+        text_align = "left",
+      },
+    diagnostics = "nvim_lsp",
+    },
+  },
 }
