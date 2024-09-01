7598da7dad999a31fedf0a12181bd8a56e736c18
1eof <100410797+1eof@users.noreply.github.com>
2024-09-01 09:37:32 +0800

    merge lazy.lua and init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
 require"config.options"
 require"config.keymaps"
-require"config.lazy"
 require"config.neovide"
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- do not automatically check for plugin updates
+  checker = { enabled = false },
+})
